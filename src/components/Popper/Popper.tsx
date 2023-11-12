73ad4483943536c68a1d5467befa9f214591c6ab
fadinouira <fadi.nouira1@gmail.com>
2023-11-12 19:12:30 +0000

    HOME-PAGE: Add Popper component

---
+++ src/components/Popper/Popper.tsx
@@ -0,0 +1,28 @@
+import ReactDOM from 'react-dom';
+import { Paper } from '..';
+import './Popper.css';
+import { PopperProps } from './Popper.type';
+import usePopper from './usePopper';
+
+function Popper({ open, anchorEl, children, onClose }: PopperProps) {
+  const { position } = usePopper(open, onClose, anchorEl);
+
+  return (
+    open &&
+    ReactDOM.createPortal(
+      <div
+        className="popper-content"
+        style={{
+          top: `${position.top + 2}px`,
+          left: `${position.left}px`,
+          width: `${position.width}px`,
+        }}
+      >
+        {<Paper className="popper-trigger">{children}</Paper>}
+      </div>,
+      document.body,
+    )
+  );
+}
+
+export default Popper;
---
+++ src/components/Popper/usePopper.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+export default function usePopper(
+  open: boolean,
+  onClose: () => void,
+  anchorEl?: HTMLElement | null,
+) {
+  const [position, setPosition] = useState({ top: 0, left: 0, width: 0 });
+
+  const updatePosition = () => {
+    if (anchorEl) {
+      const rect = anchorEl.getBoundingClientRect();
+      setPosition({ top: rect.bottom, left: rect.left, width: rect.width });
+    }
+  };
+
+  useEffect(() => {
+    updatePosition();
+    if (open) {
+      window.addEventListener('resize', updatePosition);
+      document.getElementById('root')?.addEventListener('mousedown', onClose);
+    } else {
+      window.removeEventListener('resize', updatePosition);
+    }
+
+    return () => {
+      window.removeEventListener('resize', updatePosition);
+      document.getElementById('root')?.removeEventListener('mousedown', onClose);
+    };
+  }, [open, anchorEl]);
+
+  return { position };
+}
