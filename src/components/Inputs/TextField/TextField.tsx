71b000e0547c76c800fd555c465ad0c9cce5cd6f
fadinouira <fadi.nouira1@gmail.com>
2023-11-13 22:33:15 +0000

    ADDRESS-SELECTOR: Add error to texfield

--- src/components/Inputs/TextField/TextField.tsx
+++ src/components/Inputs/TextField/TextField.tsx
@@ -3,7 +3,10 @@ import './TextField.css';
 import { TextFieldProps } from './TextField.type';
 
 const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ label, placeholder, className, value, disabled, onChange, onClick, onBlur, onFocus }, ref) => {
+  (
+    { label, placeholder, className, value, disabled, error, onChange, onClick, onBlur, onFocus },
+    ref,
+  ) => {
     return (
       <div className={`text-field-root-style ${className}`} onClick={onClick}>
         {label && <label className="text-field-label-style">{label}</label>}
@@ -11,13 +14,14 @@ const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
           type="text"
           ref={ref}
           placeholder={placeholder}
-          className="text-field-input-style"
+          className={`text-field-input-style ${error ? 'text-field-input-error-style' : ''}`}
           value={value}
           onChange={onChange}
           onFocus={onFocus}
           onBlur={onBlur}
           disabled={disabled}
         />
+        {error && <label className="text-field-error-label-style">{error}</label>}
       </div>
     );
   },
