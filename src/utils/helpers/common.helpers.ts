84cda05f24a6f3815f6de8ddc9aec453a8de49ba
fadinouira <fadi.nouira1@gmail.com>
2023-11-13 22:33:29 +0000

    ADDRESS-SELECTOR:  Doccumenting utils

--- src/utils/helpers/common.helpers.ts
+++ src/utils/helpers/common.helpers.ts
@@ -1,3 +1,9 @@
+/**
+ * Checks if the provided value is `undefined`.
+ *
+ * @param value - The value to be checked.
+ * @returns `true` if the value is `undefined`, otherwise `false`.
+ */
 export const isUndefined = (value: unknown): boolean => {
   return typeof value === 'undefined';
 };
--- src/utils/helpers/path.helpers.ts
+++ src/utils/helpers/path.helpers.ts
@@ -1,3 +1,10 @@
+/**
+ * Joins an array of path segments into a single path string.
+ *
+ * @param paths - Array of path segments to be joined.
+ * @param isCreatePath - Optional. If true, creates a relative path without a leading slash.
+ * @returns The joined path string.
+ */
 export const joinPaths = (paths: string[], isCreatePath?: boolean): string => {
   let joinedPath = isCreatePath ? '' : '/';
   for (const path of paths) {
